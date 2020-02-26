993c90a77ce1a02dea21d9111db815bd9a09e51e
Harry Hammond <harry_hammond@msn.com>
2020-02-26 16:10:34 -0700

    Initial load of this project to local Git repo

---
+++ IDStore.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class IDStore
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnExit = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells
+        Me.DataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.Disable
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle1.NullValue = " "
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.DataGridView1.DefaultCellStyle = DataGridViewCellStyle1
+        Me.DataGridView1.Location = New System.Drawing.Point(13, 13)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1094, 561)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Red
+        Me.btnExit.ForeColor = System.Drawing.Color.White
+        Me.btnExit.Location = New System.Drawing.Point(1025, 580)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'IDStore
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1119, 613)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "IDStore"
+        Me.Text = "Generic ID Store"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnExit As Button
+End Class
---
+++ IDStore.vb
@@ -0,0 +1,61 @@
+﻿Public Class IDStore
+    Public FName As String = "./IDStore.csv"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim tablename As String = "export"
+        Dim DataSet As DataSet = New DataSet()
+        DataSet.Tables.Add(tablename)
+        DataSet.Tables(tablename).Columns.Add("Common Name")
+        DataSet.Tables(tablename).Columns.Add("Web Location")
+        DataSet.Tables(tablename).Columns.Add("ID")
+        DataSet.Tables(tablename).Columns.Add("PWD")
+        DataSet.Tables(tablename).Columns.Add("Notes")
+
+        Dim SR As System.IO.StreamReader = New System.IO.StreamReader(FName)
+        Dim allData As String = SR.ReadToEnd()
+        Dim rows As String() = allData.Split(vbCrLf) '("\r".ToCharArray())
+        Dim incr1 As Integer = 0
+        For Each r As String In rows
+            r = r.Trim(vbLf).Trim
+            Dim items As String() = r.Split(",")
+                If items(0) <> vbNullString And items(0) <> Nothing Then
+                    DataSet.Tables(tablename).Rows.Add(items)
+                End If
+
+            incr1 += 1
+        Next
+        SR.Close()
+        SR.Dispose()
+
+        DataGridView1.DataSource = DataSet.Tables(0).DefaultView
+        Me.Refresh()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        SaveDataGrid()
+        Me.Close()
+    End Sub
+    Private Sub SaveDataGrid()
+        Dim textstr As New System.Text.StringBuilder()
+        For x As Integer = 0 To DataGridView1.Rows.Count - 1
+            If IsNothing(DataGridView1.Rows(x).Cells(0).Value) = False Then
+                For v As Integer = 0 To DataGridView1.Columns.Count - 1
+                    'extracting cell value from 0 to 9 and add it on list
+                    If v > 0 Then
+                        textstr.Append(",")
+                    End If
+                    'If IsNothing(DataGridView1.Rows(x).Cells(v).Value) Then
+                    'textstr.Append(" ")
+                    'Else
+                    textstr.Append(DataGridView1.Rows(x).Cells(v).Value.ToString())
+
+                Next
+            End If
+            'adding new line to text
+            textstr.AppendLine()
+        Next
+        IO.File.WriteAllText(FName, textstr.ToString())
+
+        Exit Sub
+    End Sub
+End Class
